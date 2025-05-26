da570000a297754e70576b764644eba7a5514066
tqdung11a3 <truongquocdung2004@gmail.com>
2025-05-26 18:27:14 +0700

    Update reset password page

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -6,5 +6,6 @@ router.get("/login", accountController.login);
 router.get("/register", accountController.register);
 router.get("/forgot-password", accountController.forgotPassword);
 router.get("/otp-password", accountController.otpPassword);
+router.get("/reset-password", accountController.resetPassword);
 
 module.exports = router;
