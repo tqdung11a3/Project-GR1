4d365e1a7264e4a9c6bacbc6c35ab869602bdedc
tqdung11a3 <truongquocdung2004@gmail.com>
2025-05-26 18:24:01 +0700

    Update otp password page

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -5,5 +5,6 @@ const accountController = require("../../controllers/admin/account.controller");
 router.get("/login", accountController.login);
 router.get("/register", accountController.register);
 router.get("/forgot-password", accountController.forgotPassword);
+router.get("/otp-password", accountController.otpPassword);
 
 module.exports = router;
